6a1562a61b5bdcc53f78f62d11d38f208bfddd79
uwaisalqadri <uwaisalqadri654321@gmail.com>
2020-09-18 10:17:27 +0700

    add navigation components

--- app/src/main/java/com/masuwes/runtracker/ui/MainActivity.kt
+++ app/src/main/java/com/masuwes/runtracker/ui/MainActivity.kt
@@ -3,22 +3,32 @@ package com.masuwes.runtracker.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.masuwes.runtracker.R
 import com.masuwes.runtracker.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDAO: RunDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("tag", "RUNDAO: ${runDAO.hashCode()}")
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
 
     }
 }
